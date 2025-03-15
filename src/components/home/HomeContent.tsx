03020712085a5894995bd4e83d244a29ea77cbb7
Mehmet <imehmetcn@hotmail.com>
2025-03-15 13:35:42 +0300

    Remove Hero component from homepage

--- src/components/home/HomeContent.tsx
+++ src/components/home/HomeContent.tsx
@@ -1,12 +1,10 @@
 "use client";
 
-import { Hero } from "./Hero";
 import { Stats } from "./Stats";
 
 export function HomeContent() {
   return (
     <main>
-      <Hero />
       <Stats />
     </main>
   );
